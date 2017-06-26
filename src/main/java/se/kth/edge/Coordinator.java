df5e55c660edc30a1828ba97924b7af88be9cff7
Hooman Peiro Sajjad <shps@kth.se>
2017-06-26 11:51:59 +0200

    Implemented coordinator.

--- src/main/java/se/kth/edge/Coordinator.java
+++ src/main/java/se/kth/edge/Coordinator.java
@@ -1,7 +1,148 @@
 package se.kth.edge;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by Hooman on 2017-06-24.
  */
 public class Coordinator {
+
+    private final int numEdges;
+    private final HashMap<Long, int[]> keyEdgeArrivals = new HashMap<>();
+    private final HashMap<Long, Integer> keyCoordinators = new HashMap<>();
+    private final int[] edgeKeyCounters;
+    private final HashMap<Long, int[]> newKeyArrivals = new HashMap<>();
+    private final HashMap<Integer, long[]> newKeyRemovals = new HashMap<>();
+
+    public Coordinator(int numEdges) {
+        this.numEdges = numEdges;
+        this.edgeKeyCounters = new int[numEdges];
+    }
+
+    public void registerKeys(int edgeId, long[] keys, int[] arrivals) throws Exception {
+        if (keys.length != arrivals.length) {
+            throw new Exception("Number of keys does not match number of arrivals.");
+        }
+        for (int i = 0; i < keys.length; i++) {
+            int[] arrivalsPerEdge;
+            long k = keys[i];
+            if (getNewKeyArrivals().containsKey(k)) {
+                arrivalsPerEdge = getNewKeyArrivals().get(k);
+            } else {
+                arrivalsPerEdge = new int[numEdges];
+                getNewKeyArrivals().put(k, arrivalsPerEdge);
+            }
+
+            arrivalsPerEdge[edgeId] = arrivals[i];
+        }
+    }
+
+    public void unregisterKeys(int edgeId, long[] keys) {
+        getNewKeyRemovals().put(edgeId, keys);
+    }
+
+    public Map<Long, Integer> computeNewCoordinators() throws Exception {
+        Map<Long, Integer> keyEdgeMap = applyUpdates(getNewKeyArrivals(), getNewKeyRemovals());
+        getNewKeyArrivals().clear();
+        getNewKeyRemovals().clear();
+        return keyEdgeMap;
+    }
+
+    public Map<Long, Integer> applyUpdates(HashMap<Long, int[]> newKeyArrivals, HashMap<Integer, long[]>
+            newKeyRemovals) throws Exception {
+        Map<Long, Integer> keyEdgeMap = new HashMap<>();
+        Set<Long> keysToUpdate = new HashSet<>();
+
+        for (int e : newKeyRemovals.keySet()) {
+            long[] keys = newKeyRemovals.get(e);
+            for (long k : keys) {
+                int[] edges = keyEdgeArrivals.get(k);
+                if (edges == null || edges[e] == 0)
+                    throw new Exception("Inconsistent state for the registered keys.");
+                edges[e] = 0;
+                if (keyCoordinators.containsKey(k) && keyCoordinators.get(k) == e) {
+                    keysToUpdate.add(k);
+                    keyCoordinators.remove(k);
+                    edgeKeyCounters[e] -= 1;
+                }
+            }
+        }
+
+        for (long k : newKeyArrivals.keySet()) {
+            keysToUpdate.add(k);
+            int[] newArrivalsPerEdge = newKeyArrivals.get(k);
+            int[] currentArrivalsPerEdge = keyEdgeArrivals.get(k);
+            if (currentArrivalsPerEdge == null) {
+                currentArrivalsPerEdge = new int[numEdges];
+                keyEdgeArrivals.put(k, currentArrivalsPerEdge);
+            }
+            for (int i = 0; i < numEdges; i++) {
+                if (newArrivalsPerEdge[i] > 0) {
+                    if (currentArrivalsPerEdge[i] == 0) {
+                        currentArrivalsPerEdge[i] = newArrivalsPerEdge[i];
+                    } else {
+                        throw new Exception("Inconsistent state for the registered keys.");
+                    }
+                }
+            }
+        }
+
+        // update the selected keys
+        for (long k : keysToUpdate) {
+            int[] arrivalsPerEdge = keyEdgeArrivals.get(k);
+            // Choose the edge with the minimum keys assigned before (load balancing)
+            int minCoordinator = -1;
+            int currentCoordinator = -1;
+            int minLoad = Integer.MAX_VALUE;
+            if (keyCoordinators.containsKey(k)) {
+                minCoordinator = keyCoordinators.get(k);
+                currentCoordinator = minCoordinator;
+                minLoad = edgeKeyCounters[minCoordinator];
+            }
+            for (int i = 0; i < numEdges; i++) {
+                if (arrivalsPerEdge[i] > 0) {
+                    if ((edgeKeyCounters[i] < minLoad && currentCoordinator == -1) || (edgeKeyCounters[i] + 1 <
+                            minLoad && currentCoordinator != -1)) {
+                        minLoad = edgeKeyCounters[i];
+                        minCoordinator = i;
+                    }
+                }
+            }
+            if (minCoordinator == -1 && minCoordinator != currentCoordinator) {
+                throw new Exception("Inconsistent state for the registered keys.");
+            }
+
+            if (minCoordinator == -1)
+            {
+                keyEdgeMap.put(k, -1); // No coordinator can be assigned. Edges go for oblivious approach!
+                // You can remove the key from the keyEdgeArrivals
+            } else if (minCoordinator != currentCoordinator) {
+                if (keyCoordinators.containsKey(k)) {
+                    int e = keyCoordinators.get(k);
+                    edgeKeyCounters[e] -= 1;
+                }
+                keyEdgeMap.put(k, minCoordinator);
+                edgeKeyCounters[minCoordinator] += 1;
+                keyCoordinators.put(k, minCoordinator);
+            }
+
+        }
+
+        return keyEdgeMap;
+    }
+
+    public int getNumEdges() {
+        return numEdges;
+    }
+
+    public HashMap<Long, int[]> getNewKeyArrivals() {
+        return newKeyArrivals;
+    }
+
+    public HashMap<Integer, long[]> getNewKeyRemovals() {
+        return newKeyRemovals;
+    }
 }
---
+++ src/test/java/se/kth/edge/CoordinatorTest.java
@@ -0,0 +1,121 @@
+package se.kth.edge;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hooman on 2017-06-25.
+ */
+public class CoordinatorTest {
+    @Test
+    public void registerKeys() throws Exception {
+        int numEdges = 3;
+        Coordinator c = new Coordinator(numEdges);
+        long k1 = 1;
+        long k2 = 2;
+        long k3 = 3;
+        long[] keys = new long[]{k1, k2, k3};
+        int[] arrivals = new int[]{1, 1, 1};
+        c.registerKeys(0, keys, arrivals);
+        long[] keys2 = new long[]{k1, k3};
+        int[] arrivals2 = new int[]{1, 1};
+        c.registerKeys(2, keys2, arrivals2);
+        Map<Long, int[]> map = c.getNewKeyArrivals();
+        assert map.get(k1)[0] == 1;
+        assert map.get(k1)[1] == 0;
+        assert map.get(k1)[2] == 1;
+        assert map.get(k2)[0] == 1;
+        assert map.get(k2)[1] == 0;
+        assert map.get(k2)[2] == 0;
+        assert map.get(k3)[0] == 1;
+        assert map.get(k3)[1] == 0;
+        assert map.get(k3)[2] == 1;
+    }
+
+    @Test
+    public void applyUpdates() throws Exception {
+        int numEdges = 3;
+        Coordinator c = new Coordinator(numEdges);
+        long k1 = 1;
+        long k2 = 2;
+        int[] k1Edges = new int[]{1, 0, 1};
+        int[] k2Edges = new int[]{1, 0, 1};
+        HashMap<Long, int[]> newArrivals = new HashMap<>();
+        newArrivals.put(k1, k1Edges);
+        newArrivals.put(k2, k2Edges);
+        Map<Long, Integer> keyEdgeMap = c.applyUpdates(newArrivals, new HashMap<>());
+        int e1 = 0;
+        int e2 = 1;
+        int e3 = 2;
+        int c1 = 0;
+        int c2 = 0;
+        int c3 = 0;
+        for (int e : keyEdgeMap.values()) {
+            if (e == e1) {
+                c1++;
+            } else if (e == e2) {
+                c2++;
+            } else if (e == e3) {
+                c3++;
+            }
+        }
+        assert c1 == 1;
+        assert c2 == 0;
+        assert c3 == 1;
+
+        long k3 = 3;
+        k1Edges = new int[]{0, 1, 0};
+        k2Edges = new int[]{0, 1, 0};
+        int[] k3Edges = new int[]{1, 1, 1};
+        newArrivals = new HashMap<>();
+        newArrivals.put(k1, k1Edges);
+        newArrivals.put(k2, k2Edges);
+        newArrivals.put(k3, k3Edges);
+        keyEdgeMap = c.applyUpdates(newArrivals, new HashMap<>());
+        c1 = 0;
+        c2 = 0;
+        c3 = 0;
+        for (int e : keyEdgeMap.values()) {
+            if (e == e1) {
+                c1++;
+            } else if (e == e2) {
+                c2++;
+            } else if (e == e3) {
+                c3++;
+            }
+        }
+        assert c1 == 0;
+        assert c2 == 1;
+        assert c3 == 0;
+
+        HashMap<Integer, long[]> removals = new HashMap<>();
+        removals.put(e1, new long[]{k1, k2});
+        removals.put(e3, new long[]{k1, k2});
+        keyEdgeMap = c.applyUpdates(new HashMap<>(), removals);
+        c1 = 0;
+        c2 = 0;
+        c3 = 0;
+        for (int e : keyEdgeMap.values()) {
+            if (e == e1) {
+                c1++;
+            } else if (e == e2) {
+                c2++;
+            } else if (e == e3) {
+                c3++;
+            }
+        }
+        assert c1 == 0;
+        assert c2 == 2;
+        assert c3 == 0;
+
+        removals = new HashMap<>();
+        removals.put(e2, new long[]{k1});
+        keyEdgeMap = c.applyUpdates(new HashMap<>(), removals);
+        assert keyEdgeMap.get(k1) == -1;
+    }
+
+}
